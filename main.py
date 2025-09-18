963d2a10c78851562695dfadbc728c16c770d0ae
Same-xyz <samakubor@gmail.com>
2025-09-18 12:04:58 +0100

    Refactor(main): Improved output Formating

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from stats import split_words, count_chars, sort_out
+
+file_path: str = "./books/frankenstein.txt"
 def get_books_text(file_path:str) -> str: 
     with open(file_path) as f:
         file_contents= f.read()
     return file_contents
 
 def main() -> str:
-    contents = get_books_text("./books/frankenstein.txt")
+    contents = get_books_text(file_path)
     word_count: str = split_words(contents)
-    print(f"{word_count} words found in the document")
+
+    print("============ BOOKBOT ============")
+    print(f'''Analyzing books found at {file_path}...
+----------- Word Count ----------
+Found {word_count} total words''')
+    print("--------- Character Count -------")
     
     character_count = count_chars(contents)
     sorted_list = sort_out(character_count)
     
     for item in sorted_list:
         if item["char"].isalpha():
-            print(item)
-
+            print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
 
 
 main()
